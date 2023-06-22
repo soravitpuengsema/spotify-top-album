4614b2a71d8e41a80411f09fbec66057e45ec54b
soravitpuengsema <s6301012630184@email.kmutnb.ac.th>
2023-06-23 01:33:08 +0700

    dark blue background with white border

--- showTopAlbums.js
+++ showTopAlbums.js
@@ -1,16 +1,19 @@
 function showTopAlbums(topAlbums) {
   return `
-    <svg width="800" height="332.13" xmlns="http://www.w3.org/2000/svg">
-      <foreignObject width="800" height="332.13">
+    <svg width="800" height="340.13" xmlns="http://www.w3.org/2000/svg">
+      <foreignObject width="800" height="340.13">
         <div xmlns="http://www.w3.org/1999/xhtml">
           <style>
+            @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700');
+
             .container {
-              background-color: #121212;
+              background-color: #0D1117;
               display: flex;
               justify-content: space-between;
               padding: 20px;
               border-radius: 24px;
-              font-family: Garamond, sans-serif;
+              border: 1px solid white;
+              font-family: 'Montserrat', sans-serif;
             }
             .album-item {
               display: flex;
@@ -18,17 +21,18 @@ function showTopAlbums(topAlbums) {
             }
             .album {
               color: #F9F9F9;
-              font-weight: bold;
+              font-weight: 600;
               font-size: 16px;
               text-align: center;
-              margin-top: 9px;
-              margin-bottom: 2px;
+              margin-top: 12px;
+              margin-bottom: 0px;
             }
             .artist {
+              font-weight: 400;
               color: #b3b3b3;
-              font-size: 14px;
+              font-size: 13px;
               text-align: center;
-              margin-top: 4px;
+              margin-top: 0px;
               margin-bottom: 0px;
             }
           </style>
@@ -44,14 +48,14 @@ function showTopAlbums(topAlbums) {
                     <p class="album">${album[1]}</p>
                     <p class="artist">
                       <span>${album[2]}</span>
-                      <span style="fontSize:10px"> • </span>
+                      <span style="font-size:16px"> • </span>
                       <span>${album[3]}</span>
                     </p>
                   </div>
                 </div>
               `
               )
-              .join('')}
+              .join("")}
           </div>
         </div>
       </foreignObject>
