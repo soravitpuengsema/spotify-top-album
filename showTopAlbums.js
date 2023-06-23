23890d462e654edad2b70e460c2bd1ee4b20f8fc
soravitpuengsema <s6301012630184@email.kmutnb.ac.th>
2023-06-23 20:28:51 +0700

    add fade in animation

--- showTopAlbums.js
+++ showTopAlbums.js
@@ -18,34 +18,66 @@ function showTopAlbums(topAlbums) {
               display: flex;
               flex-direction: column;
             }
+            .cover {
+              opacity: 0;
+              animation: fade-in-up-scale 1.0s forwards;
+            }
             .album {
+              opacity: 0;
               color: #F9F9F9;
               font-weight: 700;
               font-size: 16px;
               text-align: center;
               margin-top: 10px;
               margin-bottom: 0px;
+              animation: fade-in 1.0s forwards;
             }
             .artist {
+              opacity: 0;
               font-weight: 400;
               color: #b3b3b3;
               font-size: 13px;
               text-align: center;
               margin-top: 0px;
               margin-bottom: 0px;
+              animation: fade-in 1.2s forwards;
+            }
+            @keyframes fade-in-up-scale {
+              0% { 
+                opacity: 0;
+                transform: scale(0.7);
+              }
+              100% { 
+                opacity: 1;
+                transform: scale(1);
+              }
+            }
+            @keyframes fade-in {
+              0% { 
+                opacity: 0;
+              }
+              100% { 
+                opacity: 1;
+              }
             }
           </style>
           <div class="container">
             ${topAlbums
               .map(
-                (album) => `
+                (album, index) => `
                 <div class="album-item">
                   <a href="${album[5]}">
-                    <img style="width:240px;height:240px" src="${album[0]}" />
+                    <img class="cover" style="width:240px;height:240px;animation-delay: ${
+                      index * 0.5 + 0.15
+                    }s" src="${album[0]}" />
                   </a>
                   <div class="text">
-                    <p class="album">${album[1]}</p>
-                    <p class="artist">
+                    <p class="album" style="animation-delay:${
+                      index * 0.5 + 0.15
+                    }s" >${album[1]}</p>
+                    <p class="artist" style="animation-delay:${
+                      index * 0.5 + 0.15
+                    }s">
                       <span>${album[2]}</span>
                       <span style="font-size:16px"> • </span>
                       <span>${album[3]}</span>
